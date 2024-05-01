431516c96f1ff078da48d4734bd64042dc7a8cf9
John Mellet <JohnMellet@outlook.com>
2024-05-01 10:42:23 +0100

    When user interacts with seacrh input, label and logo dissapear and when the user clicks out and search is emoty they return

---
+++ src/script.js
@@ -0,0 +1,15 @@
+const searchLogo = document.querySelector(".search-logo");
+const placeHolder = document.querySelector(".search-placeholder");
+const searchContainer = document.getElementById("toolbar-search");
+
+searchContainer.addEventListener("focus", function () {
+  placeHolder.classList.add("hide");
+  searchLogo.classList.add("hide");
+});
+
+searchContainer.addEventListener("blur", function () {
+  if (searchContainer.value === "") {
+    placeHolder.classList.remove("hide");
+    searchLogo.classList.remove("hide");
+  }
+});
