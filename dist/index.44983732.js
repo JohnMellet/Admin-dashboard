680279b591a0b5952893dc55f21dd9951ee27819
John Mellet <JohnMellet@outlook.com>
2024-05-01 11:35:27 +0100

    toolbar logic complete, can refine down with functions but needs more styling and snag list

--- dist/index.44983732.js
+++ dist/index.44983732.js
@@ -1,6 +1,10 @@
 const searchLogo = document.querySelector(".search-logo");
 const placeHolder = document.querySelector(".search-placeholder");
 const searchContainer = document.getElementById("toolbar-search");
+const languageList = document.querySelector(".lang-list");
+const profileSettings = document.querySelector(".account-settings");
+const langSelector = document.getElementById("lang-selector");
+const profileSelector = document.querySelector(".toolbar-pic");
 searchContainer.addEventListener("focus", function() {
     placeHolder.classList.add("hide");
     searchLogo.classList.add("hide");
@@ -10,6 +14,12 @@ searchContainer.addEventListener("blur", function() {
         placeHolder.classList.remove("hide");
         searchLogo.classList.remove("hide");
     }
+});
+langSelector.addEventListener("click", function() {
+    languageList.classList.toggle("hide");
+});
+profileSelector.addEventListener("click", function() {
+    profileSettings.classList.toggle("hide");
 });
 
 //# sourceMappingURL=index.44983732.js.map
--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 const searchLogo = document.querySelector(".search-logo");
 const placeHolder = document.querySelector(".search-placeholder");
 const searchContainer = document.getElementById("toolbar-search");
+const languageList = document.querySelector(".lang-list");
+const profileSettings = document.querySelector(".account-settings");
+const langSelector = document.getElementById("lang-selector");
+const profileSelector = document.querySelector(".toolbar-pic");
 
 searchContainer.addEventListener("focus", function () {
   placeHolder.classList.add("hide");
@@ -12,4 +16,12 @@ searchContainer.addEventListener("blur", function () {
     placeHolder.classList.remove("hide");
     searchLogo.classList.remove("hide");
   }
+});
+
+langSelector.addEventListener("click", function () {
+  languageList.classList.toggle("hide");
+});
+
+profileSelector.addEventListener("click", function () {
+  profileSettings.classList.toggle("hide");
 });
